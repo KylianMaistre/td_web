58be3ca188da63746b1be74fbb815f96cea8b5f0
KylianMaistre <113612737+KylianMaistre@users.noreply.github.com>
2022-12-21 18:36:22 +0100

    Add files via upload

---
+++ portfolio/accueil.php
@@ -0,0 +1,20 @@
+<?php
+$accueil=yaml_parse_file("accueil.yaml");
+?>
+<!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <meta charset="utf-8">
+            <title>PORTFOLIO Kylian</title>
+            <link href="styles.css" rel="stylesheet">
+        </head>
+        <body>
+            <div>
+                <?php 
+                    echo'<h1>'.$accueil['moi']['prenom'].' '.$accueil['moi']['nom'].'</h1>';
+                    echo'<h2>'.$accueil['accroche'].'</h2>';
+                ?>
+                
+            </div>
+        </body>
+    </html>
